e479dab35deeb988346978178d5b6cc9630ec98d
Aven Stewart <agstewart@costco.com>
2024-05-29 17:35:33 -0500

    initial testing for js typing effect

---
+++ assets/js/typing.js
@@ -0,0 +1,8 @@
+<script>
+    var hiddenText = document.getElemenyById('#hidden-txt').innerHTML;
+
+    var typed = new Typed('#typed-txt', {
+      strings: [hiddenText],
+      typeSpeed: 50,
+    });
+</script>
