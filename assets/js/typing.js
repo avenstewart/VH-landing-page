a27d21cb6bad61c321a8af87b14890d5a9dcc8d7
Aven Stewart <agstewart@costco.com>
2024-05-29 17:44:01 -0500

    fixed js file

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,8 +1,7 @@
-<script>
-    var hiddenText = document.getElemenyById('#hidden-txt').innerHTML;
 
-    var typed = new Typed('#typed-txt', {
-      strings: [hiddenText],
-      typeSpeed: 50,
-    });
-</script>
+var hiddenText = document.getElemenyById('#hidden-txt').innerHTML;
+
+var typed = new Typed('#typed-txt', {
+  strings: [hiddenText],
+  typeSpeed: 50,
+});
